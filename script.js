652d7cfa28cb22ff53d588ae0e2948dadfa8dac0
imamjack2 <153788389+imamjack2@users.noreply.github.com>
2024-08-04 16:58:06 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function hello(){
+    document.getElementById("title").textContent = "hello";
+}
+
+function pizza(){
+    document.getElementById("title").textContent = "pizza";
+}
+
+function JavaScript(){
+    document.getElementById("title").textContent = "JavaScript";
+}
+function nuke(){
+    window.alert("BALLISTIC MISSILE INCOME! PLEASE SEEK SHELTER IMMEDIATELY!");
+}
